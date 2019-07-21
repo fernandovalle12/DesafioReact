4378cdbb10de82345884437f7d0e4e0d2006c988
Fernando <fernando.jvalle@hotmail.com>
2019-07-20 21:46:39 -0300

    Adicionar um Todo

---
+++ src/components/TodoForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import shortid from "shortid";
+
+export default class TodoForm extends React.Component {
+  state = {
+    text: ""
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit({
+      id: shortid.generate(),
+      text: this.state.text,
+      complete: false
+    })
+    this.setState({
+      text: ""
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input name="text" value={this.state.text} onChange={this.handleChange} placeholder="item"/>
+        <button onClick={this.handleSubmit}>Adicionar</button>
+      </form>
+    );
+  }
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+
+export default class TodoList extends React.Component {
+  state = {
+    todos: []
+  }
+
+  addTodo = todo => {
+    this.setState({
+      todos: [todo, ...this.state.todos]
+    }) 
+  }
+
+  render() {
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo}/>
+        {JSON.stringify(this.state.todos)}
+      </div>  
+    );
+  }
+}
